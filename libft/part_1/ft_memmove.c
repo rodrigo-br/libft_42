6075c50d10e6c89ce7e12bd636f24786ffa0fa5b
rodrigo-br <rodrigoab123@gmail.com>
2022-11-20 17:04:44 -0300

    fixing memmove pt2

--- libft/part_1/ft_memmove.c
+++ libft/part_1/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: ralves-b <ralves-b@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 22:38:45 by ralves-b          #+#    #+#             */
-/*   Updated: 2022/11/20 09:17:43 by ralves-b         ###   ########.fr       */
+/*   Updated: 2022/11/20 15:55:01 by ralves-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	if (!dest || !src)
 		return (dest);
-	if (src <= dest)
+	if (src > dest)
 		return(ft_memcpy(dest, src, n));
 	while (n--)
 		((unsigned char *)dest)[n] = ((unsigned char *)src)[n];
