349326b71565a390801c52139f42b4a2cace328e
Rodrigo Alves  Brito <rodrigoab123@gmail.com>
2022-12-22 13:09:32 -0300

    Update ft_lstdelone.c

--- libft/bonus/ft_lstdelone.c
+++ libft/bonus/ft_lstdelone.c
@@ -14,8 +14,9 @@
 
 void	ft_lstdelone(t_list *lst, void (*del)(void*))
 {
-	if (lst == NULL || !del)
+	if (lst == NULL || *lst == NULL)
 		return ;
-	del(lst->content);
+	if (del)
+		del(lst->content);
 	free(lst);
 }
